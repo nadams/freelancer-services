a24a75953639b65fd77dbacf13f41c76a5477ae6
Nick Adams <nadams@node-3.net>
2014-03-20 19:06:39 -0600

    Adding Options class

---
+++ DataScanner/DataScanner/Parameters/Options.cs
@@ -0,0 +1,14 @@
+﻿using CommandLine;
+using CommandLine.Text;
+
+namespace DataScanner.Parameters {
+    public class Options {
+        [Option('d', "directory", Required = true, HelpText = "Base directory of the freelancer install.")]
+        public string FreelancerDirectory { get; set; }
+
+        [HelpOption]
+        public string GetUsage() {
+            return HelpText.AutoBuild(this);
+        }
+    }
+}
--- DataScanner/DataScanner/Program.cs
+++ DataScanner/DataScanner/Program.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommandLine;
+using DataScanner.Parameters;
 
 namespace DataScanner {
     public class Program {
         public static void Main(string[] args) {
+            var options = new Options();
 
+            if(Parser.Default.ParseArguments(args, options)) {
+
+            }
         }
     }
 }

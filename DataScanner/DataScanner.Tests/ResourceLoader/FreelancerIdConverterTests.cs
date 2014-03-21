a733fb438cd50c18288240d645443e0604302f83
Nick Adams <nadams@node-3.net>
2014-03-20 19:33:04 -0600

    Adding unit tests

---
+++ DataScanner/DataScanner.Tests/ResourceLoader/FreelancerIdConverterTests.cs
@@ -0,0 +1,18 @@
+﻿using DataScanner.ResourceLoader;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataScanner.Tests.ResourceLoader {
+
+    [TestClass]
+    public class FreelancerIdConverterTests {
+
+        [TestMethod]
+        public void Convert_ValueOfZero_ReturnsZero() {
+            var converter = new FreelancerIdConverter();
+
+            var result = converter.Convert(0);
+
+            Assert.AreEqual(0, result);
+        }
+    }
+}

b8a307e29b389e09b054861de74723388ed74f11
Nick Adams <nadams@node-3.net>
2014-03-22 14:59:48 -0600

    Adding GetOrElse to OptionExtensions

---
+++ DataScanner/DataScanner.Tests/Common/OptionExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using DataScanner.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace DataScanner.Tests.Common {
+
+    [TestClass, ExcludeFromCodeCoverage]
+    public class OptionExtensionsTests {
+
+        [TestMethod]
+        public void GetOrElse_OptionIsSome_ValueReturned() {
+            var option = new Some<string>("some value");
+
+            var result = option.GetOrElse(() => "default value");
+
+            Assert.AreEqual("some value", result);
+        }
+
+        [TestMethod]
+        public void GetOrElse_OptionIsNone_DefaultValueReturned() {
+            var option = new None<string>();
+
+            var result = option.GetOrElse(() => "default value");
+
+            Assert.AreEqual("default value", result);
+        }
+    }
+}
---
+++ DataScanner/DataScanner/Common/OptionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DataScanner.Common {
+    public static class OptionExtensions {
+        public static T GetOrElse<T>(this IOption<T> option, Func<T> defaultExpression) {
+            if(option.HasValue) {
+                return option.Value;
+            }
+
+            return defaultExpression();
+        }
+    }
+}
